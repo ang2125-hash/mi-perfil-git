ea4f645085b69179cf503e2b439647e7dd79d476
angel <ang21_25@alu.uabcs.mx>
2026-08-17 08:20:36 -0700

    agregados pasatiempos al perfil

--- perfil.java
+++ perfil.java
@@ -1,6 +1,8 @@
 public class Perfil {
 public static void main(String[] args) {
-System.out.println("Nombre: [Tu Nombre Completo]");
-System.out.println("Carrera: [Tu Carrera o Semestre]");
+System.out.println("Nombre: [angel adalberto gomez flores]");
+System.out.println("Carrera: [ing de software]");
+ System.out.println("Pasatiempo 1: [jugar]");
+System.out.println("Pasatiempo 2: [ver anime]");
 }
 }
